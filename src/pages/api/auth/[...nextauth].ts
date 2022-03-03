0a34da28f587ca8d5e3962d5aeb8042e59907484
Wilson Torres <wilsontorresdesigner@gmail.com>
2022-03-02 22:09:11 -0300

    Adicionado banco de dados Fauna para guardar informaçoes de usuarios logados e integrado ao NextAuth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import {query as q} from 'faunadb'
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -9,4 +12,38 @@ export default NextAuth({
       authorization:{params:{scope:'read-user'}}
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+      } catch {
+        return false
+      }
+
+      return true
+    },
+  }
 })
