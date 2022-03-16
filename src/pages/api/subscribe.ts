3c606eff6d1e659c543c8fb45df349bc6a0591e7
Wilson Seabra <wil@taprapeixe.com.br>
2022-03-16 16:30:03 -0300

    adicionado retorno da compra

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,73 @@
+import { NextApiRequest, NextApiResponse } from "next/types";
+import {query as q} from 'faunadb'
+
+import {getSession} from 'next-auth/react'
+
+import { stripe } from '../../services/stripe'
+import { fauna } from "../../services/fauna";
+
+type User = {
+  ref: {
+    id: string;
+  }
+  data: {
+    stripe_customer_id: string;
+  }
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === `POST`) {
+    
+    const session = await getSession({req});
+
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(q.Index('user_by_email'), q.Casefold(session.user.email))
+      )
+    )
+    // console.log('faunadbuser: '+user)
+    
+    
+
+    let costumerId = user.data.stripe_customer_id;
+
+    if(!costumerId){
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        //metadata
+      })
+      // console.log('stripeCustomer: '+stripeCustomer)
+  
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id,
+            }
+          }
+        )
+      )
+      costumerId = stripeCustomer.id;
+    }
+
+    const stripeCheckoutSessions = await stripe.checkout.sessions.create({
+      customer: costumerId,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [{
+        price: 'price_1KWVegDrIcyxOxJLDpAvVoVw', quantity: 1 }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL
+
+    })
+    return res.status(200).json({sessionId: stripeCheckoutSessions.id})
+  }else{
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
+}
---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (_req:NextApiRequest, res:NextApiResponse) => {
+  console.log('evento recebido')
+  res.status(200).json({ok: true})
+}
