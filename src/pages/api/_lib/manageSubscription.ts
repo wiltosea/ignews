907a7393b838450baff7960e55d2c6e0ec9b2064
Wilson Seabra <wil@taprapeixe.com.br>
2022-03-18 09:24:38 -0300

    ajustes para checkout.session.completed

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,12 @@
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string
+  ){
+    // buscar o usuário no banco do fauna com o ID costumerID (stripe_customer_id)
+    console.log(subscriptionId, customerId);
+    
+    
+    //salvar os dados da subscription do usuário no FaunaDB
+
+
+  }
